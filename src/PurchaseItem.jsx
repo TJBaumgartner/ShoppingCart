3b167d3cf29f59e2ced987a6f461d5816f9d79bb
TJBaumgartner <tnorth6@gmail.com>
2023-08-29 21:54:41 -0700

    clicking on item brings up purchase item screen

--- src/PurchaseItem.jsx
+++ src/PurchaseItem.jsx
@@ -1,13 +1,23 @@
+/* eslint-disable react/prop-types */
 import Header from "./Header";
 import Footer from "./Footer";
 import { useLocation } from "react-router-dom";
 const PurchaseItem = () => {
   const location = useLocation()
   const {props} = location.state
+  console.log(props)
     return (
     <div className="ShopPage">
         <Header></Header>
-          
+        <div className="PurchaseContainer">
+          <img src={props.data.image}></img>
+          <div className="PurchaseDescription">
+            <h1>{props.data.title}</h1>
+            <p>{props.data.description}</p>
+            <h2>${props.data.price}</h2>
+            <h3>Rating: {props.data.rating.rate} <span>({props.data.rating.count} Reviews)</span></h3>
+          </div>
+        </div>
         <Footer></Footer>
     </div>
   );
